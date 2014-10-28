7c91426dce7a68f2bd6ad508e30740f0f15b5959
Matt Griffeth <mgriffeth9@gmail.com>
2014-10-28 15:46:09 -0400

    click and constructor working

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,12 @@
-console.log('The Iron Yard Rocks');
+var Task = function(options){
+  options = options || {};
+  this.name = options.name;
+  this.status = options.status || 'incomplete';
+  this.click = function(){
+    if(this.status === 'incomplete'){
+      this.status = 'complete'
+    } else if (this.status === 'complete'){
+      this.status = 'incomplete'
+    }
+  };
+}
--- test/spec/test.js
+++ test/spec/test.js
@@ -11,25 +11,25 @@
       beforeEach(function(){
         task = new Task();
       });
-        console.log(1)
+
 
       it('should be an instance of Task', function () {
         expect(task).to.be.an.instanceof(Task)
-        console.log(2)
+
       });
       it('should have a default status of incomplete',function(){
         expect(task.status).to.equal('incomplete');
-        console.log(3)
+
       });
       it('should have a status',function(){
         expect(task).to.have.property('status');
-        console.log(4)
+
       });
       it('should change status to complete on click',function(){
         expect(task.status).to.equal('incomplete');
         task.click(task);
         expect(task.status).to.equal('complete');
-        console.log(5)
+        
       });
     });
   });
